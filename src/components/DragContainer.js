3909b399c36055477dfb036d1500f038c7814aa6
Alejo9601 <alejandro96jm@gmail.com>
2022-07-28 12:07:00 -0300

    Modified P color and StyledDiv background color

--- src/components/DragContainer.js
+++ src/components/DragContainer.js
@@ -4,18 +4,19 @@ import BackgroundImage from "./BackgroundImage";
 const StyledDiv = styled.div`
   height: 50%;
   width: 80%;
-  border: 2px dashed #a2dbfa66;
+  border: 3px dashed #a2dbfa66;
   border-radius: 10px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  background-color: #e1e5ea80;
 `;
 const P = styled.p`
   width: 100%;
   text-align: center;
   padding: 5px;
-  color: #dddddd;
+  color: #cfd2cf;
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
     Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
 `;
