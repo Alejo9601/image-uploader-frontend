e1045e07748267ccd4d9c076a18ab6b39d32b730
Alejo9601 <alejandro96jm@gmail.com>
2022-07-28 13:18:09 -0300

    Added color blue

--- src/utils/css_vars.js
+++ src/utils/css_vars.js
@@ -1,2 +1,3 @@
+export const colorBlue = "#1363df";
 export const font = `-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
 Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;`;
