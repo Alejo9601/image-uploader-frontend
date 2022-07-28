8858b65c12b06e7357a562a35e301a4e5d89027b
Alejo9601 <alejandro96jm@gmail.com>
2022-07-28 12:09:06 -0300

    Added part of the functionality to choose file button

--- src/components/ChooseFileButton.js
+++ src/components/ChooseFileButton.js
@@ -8,15 +8,35 @@ const StyledDiv = styled.div`
   justify-content: center;
   align-items: center;
 `;
-const Button = styled.button`
+const Button = styled.div`
   margin-top: 20px;
-  padding: 10px;
-  background-color: #1363df;
-  color: white;
-  font-size: 1.2rem;
-  border: 0px;
-  border-radius: 5px;
-  cursor: pointer;
+  position: relative;
+  display: inline-block;
+
+  ::before {
+    border-radius: 5px;
+    cursor: pointer;
+    background-color: #1363df;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    content: "Choose a File";
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+    font-weight: 500;
+  }
+`;
+const StyledInput = styled.input`
+  opacity: 0;
+  width: 125px;
+  height: 32px;
+  display: inline-block;
 `;
 const P = styled.p`
   color: #dddddd;
@@ -28,7 +48,9 @@ const ChooseFileButton = ({ textButton }) => {
   return (
     <StyledDiv>
       <P>Or</P>
-      <Button type="button">{textButton}</Button>
+      <Button>
+        <StyledInput type="file" name="src-file" aria-label="Archivo" />
+      </Button>
     </StyledDiv>
   );
 };
