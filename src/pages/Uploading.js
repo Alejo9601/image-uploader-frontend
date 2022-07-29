50246e9518c0246f82cacbb1579b66db6f9385ef
Alejo9601 <alejandro96jm@gmail.com>
2022-07-28 22:24:02 -0300

    Extended FlexColumnDiv

--- src/pages/Uploading.js
+++ src/pages/Uploading.js
@@ -1,16 +1,10 @@
 import styled from "styled-components";
 import ProgressBar from "../components/ProgressBar";
-import { cardBackground, font } from "../utils/css_vars";
+import { FlexColumnDiv } from "../styled/FlexColumnDiv";
+import { font } from "../utils/css_vars";
 
-const StyledDiv = styled.div`
-  background-color: ${cardBackground};
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+const StyledDiv = styled(FlexColumnDiv)`
   padding: 30px 20px;
-  border-radius: 10px;
-  box-shadow: 0px 1px 5px 0px lightgray;
 `;
 const Status = styled.p`
   font-family: ${font};
