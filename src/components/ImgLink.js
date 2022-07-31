2fcbc91d549ccd19adda65bb87e39f0ab1e974f9
Alejo9601 <alejandro96jm@gmail.com>
2022-07-31 12:17:57 -0300

    Modified several styles

--- src/components/ImgLink.js
+++ src/components/ImgLink.js
@@ -5,24 +5,26 @@ import { colorBlue, font } from "../utils/css_vars";
 const Wrapper = styled(FlexColumnDiv)`
   height: 25%;
   width: 100%;
+  margin: 15px 0px 40px 0px;
 `;
 const LinkContainer = styled(FlexColumnDiv)`
   background-color: #e1e5ea80;
   border-radius: 10px;
-  width: 80%;
+  height: 35px;
+  width: 85%;
   flex-direction: row;
   padding: 1px;
   border: 1px solid lightgray;
 `;
 const TextArea = styled.textarea`
-  width: 80%;
+  width: 75%;
   resize: none;
   background-color: transparent;
   outline: none;
   border: 0px;
 `;
 const CopyButton = styled.button`
-  width: 20%;
+  width: 25%;
   height: 100%;
   border-radius: 10px;
   background-color: ${colorBlue};
